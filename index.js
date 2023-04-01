aa878b5ec2be79a8f0fa4a5b801c7d1da790055b
Maxime Mondini <mh-mondini@hotmail.com>
2023-04-01 14:02:00 -0400

    added ts files and complied file to js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const input1 = document.getElementById('num1');
-const input2 = document.getElementById('num2');
-const button = document.getElementById('addButton');
-
-const resultBox = document.getElementById('resultBox');
-
-const sum = (num1, num2) => {
-  if (typeof num1 === 'number' && typeof num2 === 'number') {
-    return num1 + num2;
-  } else {
-    return +num1 + +num2;
-  }
+var input1 = document.getElementById('num1');
+var input2 = document.getElementById('num2');
+var button = document.getElementById('addButton');
+var resultBox = document.getElementById('resultBox');
+var sum = function (num1, num2) {
+  return +num1 + +num2;
 };
-
-button.addEventListener('click', (event) => {
+button.addEventListener('click', function (event) {
   event.preventDefault();
-
-  result = sum(input1.value, input2.value);
-  resultBox.textContent = result;
+  var result = sum(Number(input1.value), Number(input2.value));
+  resultBox.textContent = String(result);
 });

fbab27dfa66d30501783f652c1209aa537f03ecc
Maxime Mondini <mh-mondini@hotmail.com>
2023-04-01 14:33:30 -0400

    fix bugs

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var input2 = document.getElementById('num2');
 var button = document.getElementById('addButton');
 var resultBox = document.getElementById('resultBox');
 var sum = function (num1, num2) {
-  return +num1 + +num2;
+    return +num1 + +num2;
 };
 button.addEventListener('click', function (event) {
-  event.preventDefault();
-  var result = sum(Number(input1.value), Number(input2.value));
-  resultBox.textContent = String(result);
+    event.preventDefault();
+    var result = sum(Number(input1.value), Number(input2.value));
+    resultBox.textContent = String(result);
 });
--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-// const input1 = document.getElementById('num1')! as HTMLInputElement;
-// const input2 = document.getElementById('num2')! as HTMLInputElement;
-// const button = document.getElementById('addButton')! as HTMLButtonElement;
+const input1 = document.getElementById('num1')! as HTMLInputElement;
+const input2 = document.getElementById('num2')! as HTMLInputElement;
+const button = document.getElementById('addButton')! as HTMLButtonElement;
 
-// const resultBox = document.getElementById('resultBox') as HTMLHeadingElement;
+const resultBox = document.getElementById('resultBox') as HTMLHeadingElement;
 
-// const sum = (num1: number, num2: number): number => {
-//   return +num1 + +num2;
-// };
+const sum = (num1: number, num2: number): number => {
+  return +num1 + +num2;
+};
 
-// button.addEventListener('click', (event) => {
-//   event.preventDefault();
+button.addEventListener('click', (event) => {
+  event.preventDefault();
 
-//   const result = sum(Number(input1.value), Number(input2.value));
-//   resultBox.textContent = String(result);
-// });
+  const result = sum(Number(input1.value), Number(input2.value));
+  resultBox.textContent = String(result);
+});

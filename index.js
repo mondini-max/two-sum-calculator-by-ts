d68f61670910456e615f7beb1799c019c82abf6e
Maxime Mondini <mh-mondini@hotmail.com>
2023-04-01 13:32:19 -0400

    check typeof input value and change it to number

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const button = document.getElementById('addButton');
 
 const resultBox = document.getElementById('resultBox');
 
-const sum = (num1, num2) => num1 + num2;
+const sum = (num1, num2) => {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2;
+  } else {
+    return +num1 + +num2;
+  }
+};
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
